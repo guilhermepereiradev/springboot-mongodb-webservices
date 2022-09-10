b799c6fa5f46b8bfb3507620016d7a84412eebe6
guilhermepereiradev <guilhermepereira1609@hotmail.com>
2022-09-10 17:38:07 -0300

    consulta com varios criterios

--- src/main/java/com/oxd/springbootmongodb/resource/PostResource.java
+++ src/main/java/com/oxd/springbootmongodb/resource/PostResource.java
@@ -2,10 +2,13 @@ package com.oxd.springbootmongodb.resource;
 
 import com.oxd.springbootmongodb.domain.Post;
 import com.oxd.springbootmongodb.services.PostServices;
+import org.springframework.data.repository.config.RepositoryConfigurationSourceSupport;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 
@@ -28,5 +31,10 @@ public class PostResource {
     @GetMapping("/titlesearch")
     public ResponseEntity<List<Post>> findByTitle(@RequestParam String text){
         return ResponseEntity.status(HttpStatus.OK).body(postServices.findByTitle(text));
+    }
+
+    @GetMapping("/fullsearch")
+    public ResponseEntity<List<Post>> fullSearch(@RequestParam String text, @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date minDate, @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date maxDate){
+        return ResponseEntity.status(HttpStatus.OK).body(postServices.fullSearch(text, minDate, maxDate));
     }
 }
